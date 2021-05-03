142688bbb51726bd2625ce5b657c39e86f78fcc9
Muhammad Jefri Saputra <80145188+mjefris16@users.noreply.github.com>
2021-05-03 21:50:27 +0700

    Add files via upload

---
+++ I0320066_soal1_tugas10.py
@@ -0,0 +1,10 @@
+#1. Membuat Direktori
+
+import os
+
+def main():
+    os.mkdir("Tugas10")
+
+ifname = "main"
+
+main()
---
+++ I0320066_soal4_tugas10.py
@@ -0,0 +1,10 @@
+#4. Mengubah Nama Direktori
+
+import os
+
+def main():
+    os.rename("Tugas10","Muhammad Jefri Saputra_I0320066")
+
+ifname = "main"
+
+main()
